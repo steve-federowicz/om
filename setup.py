34fd9d6e4824ae90877fb3a1f782cafaa4c34eb1
Steve Federowicz <sfederow@ucsd.edu>
2014-10-20 23:27:34 -0700

    fixed setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='OME Framework',
-    version='0.1dev',
+    version='0.0.1dev',
     packages=['om',],
     license='MIT License',
     long_description=open('README.md').read(),
